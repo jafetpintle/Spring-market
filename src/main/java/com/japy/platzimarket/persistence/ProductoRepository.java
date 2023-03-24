42a9359f1813cdb347e11e952538e349c0f0186a
jafet <jafetpintle@gmail.com>
2023-03-23 20:48:33 -0600

    Bugfix

--- src/main/java/com/japy/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/japy/platzimarket/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.japy.platzimarket.persistence;
 
 import com.japy.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.japy.platzimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,6 +21,18 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 
 

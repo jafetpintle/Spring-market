0a4eefb7929253abb5dbe266a404563d3a6cdf25
jafet <jafetpintle@gmail.com>
2023-03-23 20:49:42 -0600

    Mapper Category and Product

---
+++ src/main/java/com/japy/platzimarket/persistence/mapper/ProductMapper.java
@@ -0,0 +1,34 @@
+package com.japy.platzimarket.persistence.mapper;
+
+import com.japy.platzimarket.domain.Product;
+import com.japy.platzimarket.persistence.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+//Tenemos que definir que es un mapper
+//Indicamos que uste tambien mapper de category
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+    @Mappings({//Referenciamos de donde va a tomar los datos
+            @Mapping(source = "idProducto",target = "productId"),
+            @Mapping(source = "nombre",target = "name"),
+            @Mapping(source = "idCategoria",target = "categoryId"),
+            @Mapping(source = "precioVenta",target = "price"),
+            @Mapping(source = "cantidadStock",target = "stock"),
+            @Mapping(source = "estado",target = "active"),
+            @Mapping(source = "categoria",target = "category")
+    })
+    Product toProduct(Producto producto);
+    List<Producto> toProducts(List<Producto> productos);
+
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarras", ignore = true)
+    Producto toProducto(Product product);
+
+
+}

dcec4317cbcf5b5623d44cbccb7a6bc8c3b1b468
jafet <jafetpintle@gmail.com>
2023-03-24 10:35:58 -0600

    Product rest Controller

---
+++ src/main/java/com/japy/platzimarket/web/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.japy.platzimarket.web.controller;
+
+import com.japy.platzimarket.domain.Product;
+import com.japy.platzimarket.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public List<Product> getAll(){
+        return productService.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId){
+        return productService.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId){
+        return productService.getByCategory(categoryId);
+    }
+
+    public Product save(Product product){
+        return productService.save(product);
+    }
+
+    public boolean delete(int productId){
+        return productService.delete(productId);
+    }
+
+    }

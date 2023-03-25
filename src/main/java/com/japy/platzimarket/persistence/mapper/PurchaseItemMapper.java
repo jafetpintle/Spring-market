9df84f586d7df3150cd0260b6f65f6e372eff532
jafet <jafetpintle@gmail.com>
2023-03-25 16:08:36 -0600

    Mapping purchaseItem and Purchase

---
+++ src/main/java/com/japy/platzimarket/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,27 @@
+package com.japy.platzimarket.persistence.mapper;
+
+import com.japy.platzimarket.domain.PurchaseItem;
+import com.japy.platzimarket.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
+public interface PurchaseItemMapper {
+    @Mappings({
+            @Mapping(source = "id.idProducto", target = "productId"),
+            @Mapping(source = "cantidad", target = "quantity"),
+            @Mapping(source = "total", target = "total"),
+            @Mapping(source = "estado", target = "active"),
+    })
+    PurchaseItem toPurchaseItem(ComprasProducto comprasProducto);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "id.idCompra", ignore = true),
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true)
+    })
+    ComprasProducto toComprasProducto(PurchaseItem purchaseItem);
+}
